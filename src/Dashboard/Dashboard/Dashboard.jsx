b89d6015a539d2a18945c88f4b156ab11179475b
ashikmahmud <ashikmamud187@gmail.com>
2022-05-09 00:27:11 +0600

    added dashboard

--- src/Dashboard/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard/Dashboard.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./dashboard.css";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="dashboard">
+      <h1>Welcome to the dashboard.</h1>
+    </div>
+  );
 };
 
 export default Dashboard;
